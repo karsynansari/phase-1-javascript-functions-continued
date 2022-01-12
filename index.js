a7a539bf4782d291cd7f48c22866548bdf1bdd15
karsyn ansari <karsyn.ansari@gmail.com>
2022-01-11 17:19:04 -0700

    commit tree 5da6334f272207e2d194ba85657cf538cab45a60

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+// function saturdayFun(activity = "roller-skate") {
+//   return `This Saturday, I want to ${activity}!`;
+// }
+// const mondayWork = function (job = "go to the office") {
+//   return `This Monday, I will ${job}.`;
+// };
+
+// function wrapAdjective(flair = "*") {
+//   return function (youAre = "special") {
+//     return `You are ${flair}${youAre}${flair}!`;
+//   };
+// }
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(work = "go to the office") {
+  return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(param1 = "*") {
+  return function (adjective = "special") {
+    return `You are ${param1}${adjective}${param1}!`;
+  };
+}
